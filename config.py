afe0a886efccbb9387aea37451ed93613c43e671
blank0610 <125758092+blank0610@users.noreply.github.com>
2026-04-02 14:50:27 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+class DatabaseConfig:
+    HOST = "mysql.cs.bangor.ac.uk"
+    PORT = 3306
+    USER = "hnh24pbz"
+    PASSWORD = "9921e7557d"
+    DATABASE = "hnh24pbz"
+
+
+class GeminiConfig:
+    API_KEY = "AIzaSyB5z17BtglAJ7MaFjrGkpa7aZEGzXyaa7s"
+    MODEL = "gemini-2.5-flash"
